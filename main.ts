009cb31ed8a1eb36d7c88c5340d172311d55c366
BionicSystemSolutions <71697559+BionicSystemSolutions@users.noreply.github.com>
2020-09-22 14:32:15 +0200

    test1

--- main.ts
+++ main.ts
@@ -1,1 +1,25 @@
 
+namespace sensors {
+    class Asl10SoundSensor extends sensors.internal.IICSensor {
+
+        getInfo(): number[] {
+            this.transaction(8, [256, 0, 256, 0, 256, 0], 6);
+
+            control.waitMicros(60000);
+            let buf = this.getBytes();
+            let array = [0, 0, 0, 0, 0, 0]
+            for (let i = 0; i < buf.length; i++) {
+                array[i] = buf[i];
+            }
+            return array;
+            //return this.getBytes()[0];
+        }
+
+
+
+    }
+    export const asl10Sound1 = new Asl10SoundSensor(1)
+    export const asl10Sound2 = new Asl10SoundSensor(2)
+    export const asl10Sound3 = new Asl10SoundSensor(3)
+    export const asl10Sound4 = new Asl10SoundSensor(4)
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,37 @@
 // tests go here; this will not be compiled when this package is used as a library
+
+brick.showString("A. S. L.", 1)
+let counter = 0;
+
+
+forever(function () {
+
+    let id = sensors.asl10Sound1.getInfo();
+
+
+    for (let i = 0; i < id.length; i++) {
+        let x = i + 2;
+        brick.showNumber(id[i], x);
+
+    }
+
+    /*
+    sensors.internal.transactionIIC(1, 8, [0, 0, 0, 0, 0, 0], 4);
+    control.waitMicros(60000);
+    let text = sensors.internal.readIICID(8);
+
+    brick.showNumber(text.length, 4);
+    brick.showString(text, 5);*/
+
+
+
+
+
+    brick.showNumber(counter, 10);
+    counter++;
+
+})
+
+
+
+
