5b3d9399499e4a66acacf8247c6b61a307855a87
BionicSystemSolutions <71697559+BionicSystemSolutions@users.noreply.github.com>
2020-10-20 14:04:08 +0200

    grouptest

--- main.ts
+++ main.ts
@@ -1,23 +1,25 @@
 
-namespace sensors {
+namespace sensorstest {
     class Asl10SoundSensor extends sensors.internal.IICSensor {
 
-
+        // constructor og _deviceType behøves ikke, men jeg så dog 
+        // at andre lego sensorer har dem. 
         /*
         constructor(port: number) {
             super(port)
 
         }*/
-
         /*_deviceType() {
             return DAL.DEVICE_TYPE_NXT_IIC
         }*/
 
+        // for bytes over i2c
         getData() {
             this.transaction(8, [0], 6)
             return this.getBytes()
         }
 
+        // udregner "vinklen" udfra data
         getAngle() {
 
             let buf = this.getData();
@@ -25,23 +27,32 @@ namespace sensors {
             let sumR = (buf[2] << 8) + (buf[3] & 0xff);
             let sumX = (buf[4] << 8) + (buf[5] & 0xff);
 
+            // printer en masse ting. Skal udkommenteres
             brick.showValue("SumL", sumL, 1);
-
             brick.showValue("SumR", sumR, 2);
-
             brick.showValue("SumX", sumX, 3);
+            brick.showValue("buf0", buf[0], 5);
+            brick.showValue("buf1", buf[1], 6);
+
 
 
             let a2 = 0.0;
-            if (sumX != 0) {
+            // eventuelt fjerne max-grænse
+            // max-grænse er der, fordi der opstod overførsler på over 65000
+            // som enten tyder på overflow eller en løs forbindelse, 
+            // da det kom, når sensoren blev udsat for rystelse/vibrationer.
+            let max = 2000;
+            if (sumX != 0 && sumL < max && sumR < max) {
                 a2 = (sumL - sumR) / sumX
             }
             return a2;
         }
 
+    // denne skal sende sensor id, den virker dog kun hvis der står lego i den.
+    // måske en sammenhæng mellem det og at internal.sensors.readIICID() læses til
+    // LegoTempStore%s
         _IICId() {
-
-            return 'LEGO'; // denne skal den ahave for at den kan have en aktiv sensor
+            return 'LEGO'; 
         }
     }
 
