4cb3fb75a121da09d1210be11390adc7a9d2a47d
BionicSystemSolutions <71697559+BionicSystemSolutions@users.noreply.github.com>
2020-10-20 10:29:54 +0200

    20-10-2020

--- main.ts
+++ main.ts
@@ -2,23 +2,49 @@
 namespace sensors {
     class Asl10SoundSensor extends sensors.internal.IICSensor {
 
-        getInfo(): number[] {
-            this.transaction(8, [5, 5, 5, 5, 5, 5], 6);
 
-            control.waitMicros(100000);
-            let buf = this.getBytes();
-            
-            let array = [0, 0, 0, 0, 0, 0]
-            for (let i = 0; i < buf.length; i++) {
-                array[i] = buf[i];
-            }
-            return array;
-            //return this.getBytes()[0];
+        /*
+        constructor(port: number) {
+            super(port)
+
+        }*/
+
+        /*_deviceType() {
+            return DAL.DEVICE_TYPE_NXT_IIC
+        }*/
+
+        getData() {
+            this.transaction(8, [0], 6)
+            return this.getBytes()
         }
 
+        getAngle() {
+
+            let buf = this.getData();
+            let sumL = (buf[0] << 8) + (buf[1] & 0xff);
+            let sumR = (buf[2] << 8) + (buf[3] & 0xff);
+            let sumX = (buf[4] << 8) + (buf[5] & 0xff);
+
+            brick.showValue("SumL", sumL, 1);
+
+            brick.showValue("SumR", sumR, 2);
+
+            brick.showValue("SumX", sumX, 3);
 
 
+            let a2 = 0.0;
+            if (sumX != 0) {
+                a2 = (sumL - sumR) / sumX
+            }
+            return a2;
+        }
+
+        _IICId() {
+
+            return 'LEGO'; // denne skal den ahave for at den kan have en aktiv sensor
+        }
     }
+
     export const asl10Sound1 = new Asl10SoundSensor(1)
     export const asl10Sound2 = new Asl10SoundSensor(2)
     export const asl10Sound3 = new Asl10SoundSensor(3)
--- test.ts
+++ test.ts
@@ -1,53 +1,118 @@
 // tests go here; this will not be compiled when this package is used as a library
 
-brick.showString("A. S. L.", 1)
-let counter = 0;
+brick.buttonDown.onEvent(ButtonEvent.Pressed, function () {
+
+    motors.largeBC.tank(-75, 75);
 
 
-forever(function () {
+    /*let i2cID1 = sensors.internal.readIICID(0);
+    brick.showString(i2cID1, 11);
+    let i2cID2 = sensors.internal.readIICID(2);
+    brick.showString(i2cID2, 12);*/
 
-    // test om sensorere overhovedet er aktive. 
-    // se om andr ehar lykkedes at implementere ne i2c sensor i makecode.
+})
+brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
 
-
-
-
-    //let id = sensors.asl10Sound1.getInfo();
-
-
-
-    sensors.internal.transactionIIC(1, 8, [0, 0, 0, 0, 0, 0], 6);
-    
-    //let x = sensors.internal.getActiveSensors();
-    //
+    motors.largeBC.tank(0, 0);
 
 
-    //sensors.internal.iicsensor.transaction(8, [0, 0, 0, 0, 0, 0], 6);
-    control.waitMicros(100000);
-    let buf = sensors.internal.getIICBytes(1, 6);
+    /*let i2cID1 = sensors.internal.readIICID(0);
+    brick.showString(i2cID1, 11);
+    let i2cID2 = sensors.internal.readIICID(2);
+    brick.showString(i2cID2, 12);*/
 
-    let text = sensors.internal.bufferToString(buf);
+})
 
-    if (!text.compare("")) {
-        brick.showString("Ingen tekst!", 4);
+brick.buttonUp.onEvent(ButtonEvent.Pressed, function () {
+
+    brick.setStatusLight(StatusLight.Green);
+
+    let x = sensors.internal.getActiveSensors();
+
+    let sum = 0;
+    for (let i = 0; i < x.length; i++) {
+        sum++;
     }
 
-    brick.showString(text, 3);
+    brick.showValue("Aktive sensorer", sum, 6)
+
+    //sensors.asl10Sound2.poke(); Kan ikke aktivere soundsensor selvom den kan aktivere andre slags.
+    //sensors.infrared1.poke(); kan kun bruges til at aktivere infrarød sensoren
+    if (sum == 1) {
+        let var1 = x.get(0).id();
+        brick.showValue("aktiv sensor", var1, 7)
+        let portAktiv = x.get(0).port();
+        let i2cID1 = sensors.internal.readIICID(portAktiv);
+        brick.showValue("port", portAktiv, 8);
+        brick.showString(i2cID1, 9);
+
+        sensors.internal.setIICMode(0, 0, 0)
+
+    } else if (sum == 2) {
+        let var1 = x.get(0).id();
+        brick.showValue("aktiv sensor", var1, 7)
+        let var2 = x.get(0).id();
+        brick.showValue("aktiv sensor", var2, 8)
+    }
 
 
     /*
-    sensors.internal.transactionIIC(1, 8, [0, 0, 0, 0, 0, 0], 4);
-    control.waitMicros(60000);
-    let text = sensors.internal.readIICID(8);
+        let i2cID1 = sensors.internal.readIICID(1);
+    
+        let i2cID2 = sensors.internal.readIICID(2);
+    
+        let data = sensors.asl10Sound2.id();
+    
+        let data2 = sensors.infrared1.id();
+    
+        brick.showString(i2cID1, 8);
+    
+        brick.showString(i2cID2, 9)
+    
+        brick.showNumber(data, 10);
+        brick.showNumber(data2, 11);
+    
+    */
 
-    brick.showNumber(text.length, 4);
-    brick.showString(text, 5);*/
+
+
+})
+
+
+let counter = 0;
+let threshold = 1;
+
+forever(function () {
 
 
 
+    let angle = sensors.asl10Sound2.getAngle();
 
 
-    brick.showNumber(counter, 10);
+
+    if (angle > 3) { angle = 3; }
+    if (angle < -3) { angle = -3; }
+
+    let speed = angle * 25;
+
+
+
+    //if (counter % 10 == 0) {
+    if (angle > threshold) {
+        motors.largeBC.tank(-speed, speed);
+    } else if (angle < -threshold) {
+        motors.largeBC.tank(-speed, speed);
+    } else {
+        motors.largeBC.tank(0, 0);
+    }
+    //} else if (counter % 10 == 4) {
+    //   motors.largeBC.tank(0, 0);
+    // }
+
+
+    brick.showValue("Angle", angle, 4);
+
+    brick.showNumber(counter, 12);
     counter++;
 
 })
